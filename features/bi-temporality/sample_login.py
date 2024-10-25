032082d662d01333e6151e458f96db41931145dc
Concourse <concourse@finbourne.com>
2024-10-25 14:09:44 +0000

    Publishing notebooks
    Signed-off-by: Concourse <concourse@finbourne.com>

---
+++ features/bi-temporality/sample_login.py
@@ -0,0 +1,81 @@
+import lusid
+import lusid.models as models
+import os
+import json
+from urllib.request import pathname2url
+import requests
+from collections import UserString
+from datetime import datetime, timedelta, time
+
+
+class RefreshingToken(UserString):
+
+    def __init__(self):
+
+        token_data = {
+            "expires": datetime.now(),
+            "current_access_token": ''
+        }
+
+        current_access_token = ''
+
+        def get_token():
+            if token_data['expires'] <= datetime.now():
+                with open("./../access_token.txt", "r") as access_token_file:
+                    token_data['current_access_token'] = access_token_file.read()
+
+                token_data['expires'] = datetime.now() + timedelta(seconds=120)
+
+            return token_data['current_access_token']
+
+        self.access_token = get_token
+
+    def __getattribute__(self, name):
+        token = object.__getattribute__(self, 'access_token')()
+        if name == 'data':
+            return token
+        return token.__getattribute__(name)
+
+
+def authenticate_secrets():
+    class LusidApi():
+
+        def __init__(self, client):
+            self.aggregation = lusid.AggregationApi(client)
+            self.metadata = lusid.ApplicationMetadataApi(client)
+            self.corporate_action_sources = lusid.CorporateActionSourcesApi(client)
+            self.data_types = lusid.DataTypesApi(client)
+            self.derived_transaction_portfolios = lusid.DerivedTransactionPortfoliosApi(client)
+            self.instruments = lusid.InstrumentsApi(client)
+            self.login = lusid.InstrumentsApi(client)
+            self.portfolio_groups = lusid.PortfolioGroupsApi(client)
+            self.portfolios = lusid.PortfoliosApi(client)
+            self.property_definitions = lusid.PropertyDefinitionsApi(client)
+            self.quotes = lusid.QuotesApi(client)
+            self.reconciliations = lusid.ReconciliationsApi(client)
+            self.reference_portfolios = lusid.ReferencePortfolioApi(client)
+            self.results = lusid.ResultsApi(client)
+            self.schemas = lusid.SchemasApi(client)
+            self.scopes = lusid.ScopesApi(client)
+            self.search = lusid.SearchApi(client)
+            self.transaction_portfolios = lusid.TransactionPortfoliosApi(client)
+            self.cut_labels = lusid.CutLabelDefinitionsApi(client)
+
+    environment = os.getenv("FBN_DEPLOYMENT_ENVIRONMENT", None)
+
+    if environment == "JupyterNotebook":
+        api_url = os.getenv("FBN_LUSID_API_URL", None)
+        if api_url is None:
+            raise KeyError("Missing FBN_LUSID_API_URL environment variable, please set it to the LUSID base API url")
+        api_factory = lusid.utilities.ApiClientFactory(
+            token=RefreshingToken(),
+            api_url=api_url,
+            app_name="LusidJupyterNotebook")
+        return api_factory
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    secrets_path = os.path.join(dir_path, "../secrets.json")
+
+    api_factory = lusid.utilities.ApiClientFactory(api_secrets_filename=secrets_path)
+
+    return api_factory
---
+++ features/instruments/utilities/formatting_tools.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import lusid
+
+
+# Create data frame skeleton
+def new_data_frame():
+    return pd.DataFrame(columns=[
+        "Currency",
+        "Date",
+        "Activity",
+        "Value"
+    ])
+
+
+# Add open values
+def add_open_row(element, record):
+    df = new_data_frame()
+    df = pd.concat([df, pd.DataFrame([{
+        "Currency": element.currency,
+        "Date": record.effective_date,
+        "Activity": "Open",
+        "Value": record.open
+    }])]).reset_index(drop=True)
+
+    return df
+
+
+# Add close values
+def add_close_row(element, record, df):
+    df = pd.concat([df, pd.DataFrame([{
+        "Currency": element.currency,
+        "Date": record.effective_date,
+        "Activity": "Close",
+        "Value": record.close}])]).reset_index(drop=True)
+
+    return df
+
+
+# Add activites
+def add_activity_row(element, record, df, activity, value):
+    df = pd.concat([df, pd.DataFrame([{
+        "Currency": element.currency,
+        "Date": record.effective_date,
+        "Activity": activity,
+        "Value": value
+    }])]).reset_index(drop=True)
+
+    return df
+
+
+def cashladder_to_df(response) -> pd.DataFrame:
+    """
+    Formats a LUSID cash ladder response to a pandas DataFrame.
+    Parameters
+    ----------
+    response
+
+    Returns
+    -------
+    pd.DataFrame
+    """
+
+    base_df = new_data_frame()
+
+    for element in response.values:
+        shks = {shk: value.value.label_value for shk, value in element.sub_holding_keys.items()}
+
+        for record in element.records:
+            working_df = add_open_row(element, record)
+
+            for activity, value in record.activities.items():
+                working_df = add_activity_row(element, record, working_df, activity, value)
+            working_df = add_close_row(element, record, working_df)
+
+            for k, v in shks.items():
+                working_df[k] = v
+            base_df = pd.concat([base_df, working_df]).reset_index(drop=True)
+
+    return base_df
+
+
+def cash_flow_response_to_df(
+        portfolio_cash_flows_response: lusid.ResourceListOfPortfolioCashFlow,
+        sum_by_date: bool = True
+) -> pd.DataFrame:
+    """
+   Takes a LUSID response from 'GetPortfolioCashFlows' and formats pays and receives into a dataframe, adding the net
+   column.
+    """
+
+    def select_cols(
+            df: pd.DataFrame,
+            filter_col: str,
+            filter_value: str,
+            cols_to_keep: list,
+    ) -> pd.DataFrame:
+        return df[df[filter_col] == filter_value][cols_to_keep]
+
+    # Extract cash payment data from cash flow response
+    cash_flows_dict = portfolio_cash_flows_response.to_dict()
+    cash_flow_data = pd.json_normalize(cash_flows_dict["values"])
+
+    # Split pays and receives and handle -ve signage for pay outflows
+    pay_data = select_cols(
+        cash_flow_data,
+        "diagnostics.PayReceive",
+        "Pay",["payment_date", "amount", "source_transaction_id"]
+    )
+    pay_data["amount"] = pay_data["amount"].apply(lambda x: -1 * x)
+    pay_data.rename(columns={"amount": "payAmount"}, inplace=True)
+    rec_data = select_cols(
+        cash_flow_data,
+        "diagnostics.PayReceive",
+        "Receive",
+        ["payment_date", "amount", "source_transaction_id"]
+    )
+    rec_data.rename(columns={"amount": "receiveAmount"}, inplace=True)
+
+    # Merge on payment date and ignore join dupes
+    merged_df = pay_data.merge(rec_data, on=["payment_date", "source_transaction_id"])
+    merged_df.drop_duplicates(subset=["payment_date", "source_transaction_id"], keep="first", inplace=True,
+                              ignore_index=True)
+
+    # Add net flows and reduce index to dates
+    merged_df['netAmount'] = merged_df['payAmount'] + merged_df['receiveAmount']
+    merged_df["payment_date"] = merged_df["payment_date"].apply(lambda x: x.date())
+    merged_df.set_index(keys="payment_date", inplace=True)
+
+    # Aggregate sub-holdings
+    if sum_by_date:
+        merged_df = merged_df.groupby(merged_df.index).sum()
+
+    return merged_df
+
